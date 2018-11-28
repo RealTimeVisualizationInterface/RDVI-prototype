4b39f07746e4467f54e45e52e871826a60bd1d5e
Ignacio Chamorro <ignaciochg@hotmail.com>
2018-11-27 21:01:16 -0800

    initial commit add main graph, not done add empty size feature file

---
+++ sizeFeature.js
@@ -0,0 +1,25 @@
+
+//function that will output two numbers 
+
+var arr = [
+        { x: 10, y: 71 },
+        { x: 20, y: 55},
+        { x: 30, y: 50 },
+        { x: 40, y: 65 },
+        { x: 50, y: 95 },
+        { x: 60, y: 68 },
+        { x: 70, y: 28 },
+        { x: 80, y: 34 },
+        { x: 90, y: 14}
+ ]
+var min
+var max
+var lookAtLast = 200
+var limitMin
+var limitMax
+ def fas;lkdfjas(){
+        asljf
+        asfkljasdf
+        asdflasdf
+
+ }
