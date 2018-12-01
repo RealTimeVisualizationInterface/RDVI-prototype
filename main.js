34c525fa630f0b44e4cce815ce7b46b59afc20aa
Ignacio Chamorro <ignaciochg@hotmail.com>
2018-11-30 17:14:03 -0800

    added main features of grph

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// https://www.one-tab.com/page/6iMfYO8qSlayQtXkmRH6FQ
+
 //API Settings
 var rdviApi = "";
 var dataApi = "";
@@ -6,6 +8,9 @@ var dpPath = "";
 var configurationFullPath = rdviApi+settingsPath;
 var samplesFullPath = dataApi+dpPath;
 
+
+//machine variables// 
+var machineName ;//one for each graph/machine
 
 //Data Point variables
 var dpNames = []; //string
@@ -23,10 +28,10 @@ columns: [
 
 
 //Graph configuration settings variables //one for each data point
+var graphs = []; //place to store graphs
 var yAxisTopLimit = []; // float 
 var yAxisBotLimit = []; // float
-var xAxisLeft = []; // in js Date object format
-var xAxisRight = []; // in js Date object format
+var xAxisLastTimeToShow = []; // in js Date object format, var = new Date("October 13, 2014 11:15:00");
 var graphBindIDs = [
                      "#GRAPH0",
                      "#GRAPH1",
@@ -56,25 +61,39 @@ function addOneDP(graphIndex, pointValue, pointTime = ""){
 
 }
 
+function getCurrentTime(){
+   return  
+   var y = getFullYear().toString();
+   var mo = (getMonth()+1).toString();// zero indexed
+   var d = getDate().toString();
+   var h =  getHours().toString();
+   var mi = getMinutes().toString();
+   var s = getSeconds().toString();
+   return y+"-"+mo+"-"+d+" "+h+":"+mi+":"+s;
+   // %Y-%m-%d %H:%M:%S
+   // match input format
+}
 
-
-//getDPConfiguration(){
+function getConfiguration(){
    //call api get data
    /*
       call: get quantity and names of data points  
+      ///machineName.push("name");//does not apply
       for every dp in call
+
          dpNames[index] = name; //string
          upperLimits[index]= float; // float
          lowerLimits[index] = float; // float
          dpYValues.push([]);//push empty array
-         dpYValues[index].push(name); // array will have format of [name, val0,val1,...], initDP() will initialize it
+         // does not apply since we are only doing one machine
+         // dpYValues[index].push(machineName[i]); // array will have format of [machineName, val0,val1,...], initDP() will initialize it
          dpXValues.push([]);//push empty array
          dpXValues[index].push("date"); // array will have format of [name, val0,val1,...], initDP() will initialize it         
          dpRefreshRate[index] = rate;
 
    */
 
-//}
+}
 
 
 function createGraph(index){//all information has to be available before calling
@@ -84,6 +103,7 @@ function createGraph(index){//all information has to be available before calling
       bindto: graphBindIDs[index],
       data: {
          x: 'Time',
+         
          columns: [
             dpXValues[index],
             dpYValues[index],
@@ -92,24 +112,48 @@ function createGraph(index){//all information has to be available before calling
       },
       axis: {
          x: {
+            type: 'timeseries',
             label: dpNames[index],
+            localtime: false,
+            //xFormat: '%Y-%m-%d %H:%M:%S',//how to parse x 
             tick: {
-               fit: false
-            }
+                format: '%H:%M:%S',//how to display x
+                fit: true,
+                count: 5,
+
+
+            },
+            min: xAxisLastTimeToShow[index]
+
          },
          y: {
             label: 'Date Time'
          }
-      }
+      },
+      regions: [
+            {axis: 'y', start: lowerLimits[index], end: upperLimits[index], class: 'regionGood'},
+         ],
+      grid: {
+         y: {
+            show: true,
+            lines: [
+               {value: lowerLimits[index], text: 'Lower Limit', position: 'middle'},
+               {value: upperLimits[index], text: 'Upper Limit', position: 'middle'},
+
+            ]
+         },
+         x: {
+            show: true,
+
+         }
+    }
    });
 }
 
 
 
 
-/*data: {
-  labels: true
-}
+/*
 
 chart.axis.min({
   x: -10,
@@ -122,7 +166,8 @@ chart.axis.min({
 
 
 
-function updateGraph(index){
+function updateGraph(index){//add value to graph
+   console.log("updating graph")
    /*
 
       for every value in 
@@ -130,26 +175,54 @@ function updateGraph(index){
 
    */
 
+dpYValues[0].push(5.1);//dp0
+dpXValues[0].push(new Date(new Date("October 13, 2014 11:13:00").getMilliseconds() + 1000));//dp0
+console.log(dpXValues[0]);
 }
 
 
 
 
 
-
-//testing value 
+//manual values
+machineName = "Machine 1";
 dpNames.push("Voltage");//dp0
-upperLimits.push(7.0); //dp0
-lowerLimits.push(-1.0);//dp0
-dpYValues.push(["voltage",3.5,3.6,3.8,3.0,2.5]);//dp0
-dpXValues.push(["Time",1,2,3,4,5]);//dp0
+upperLimits.push(5.2); //dp0
+lowerLimits.push(4.8);//dp0
+
+
+// dpYValues.push([machineName]);//dp0
+// dpXValues.push(["Time"]);//dp0
+
+
+dpYValues.push([machineName,3.5,3.6,3.8,5.0,4.9,5.12,3.0,2.5,5.1]);//dp0
+dpXValues.push(["Time",
+                  new Date("October 13, 2014 11:13:00"),
+                  new Date("October 13, 2014 11:14:00"),
+                  new Date("October 13, 2014 11:15:00"),
+                  new Date("October 13, 2014 11:16:00"),
+                  new Date("October 13, 2014 11:17:00"),
+                  new Date("October 13, 2014 11:18:00"),
+                  new Date("October 13, 2014 11:19:00"),
+                  new Date("October 13, 2014 11:20:00"),
+                  new Date("October 13, 2014 11:21:00")]);//dp0
+
 dpRefreshRate.push(1000);//for dp0
 
+xAxisLastTimeToShow.push(new Date("October 13, 2014 11:18:00"));//sp0, valuees before this will not show anymore
+//xAxisLastTimeToShow.push(new Date(getCurrentTime()));//sp0, valuees before this will not show anymore
 
-var myGraph = createGraph(0);
 
 
-for (let index = 0; index < dpNames.length; ++index) {//call all the refrsh rates 
+
+getConfiguration();
+/*
+code that will settup plage layout depending on how many graphs we have 
+*/
+
+for (let index = 0; index < dpNames.length; ++index) {// make everything
+
+   graphs.push(createGraph(index));
    setInterval(updateGraph(index),dpRefreshRate[index]);
 }
 
